3a8f3dff5fde0d9d8e5e61b0c938b858aef0c30b
Carlo Holl <carloholl@gmail.com>
2020-05-15 17:55:21 +0000

    Ignore extensions in trigger query

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,3 +1,9 @@
+WITH extension_oids AS (
+  SELECT objid AS oid FROM pg_depend d
+  WHERE
+      d.refclassid = 'pg_extension'::regclass
+      AND d.classid = 'pg_proc'::regclass
+)
 SELECT
     tg.oid AS oid,
     nsp.nspname AS "schema",
@@ -8,9 +14,11 @@ SELECT
     nspp.nspname AS proc_schema,
     tg.tgenabled AS enabled
 FROM pg_trigger tg
-JOIN pg_class cls ON cls.oid = tg.tgrelid
-JOIN pg_namespace nsp ON nsp.oid = cls.relnamespace
-JOIN pg_proc proc ON proc.oid = tg.tgfoid
-JOIN pg_namespace nspp ON nspp.oid = proc.pronamespace
+    INNER JOIN pg_class cls ON cls.oid = tg.tgrelid
+    INNER JOIN pg_namespace nsp ON nsp.oid = cls.relnamespace
+    INNER JOIN pg_proc proc ON proc.oid = tg.tgfoid
+    INNER JOIN pg_namespace nspp ON nspp.oid = proc.pronamespace
+    LEFT OUTER JOIN extension_oids e ON e.oid = proc.oid
 WHERE NOT tg.tgisinternal
+AND e.oid is null
 ORDER BY schema, table_name, name;
